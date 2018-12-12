477ff02e321290435228b119dacc37e28b7369a1
Shan Zuidema <shan.zuidema@unh.edu>
2018-12-12 17:08:39 -0500

    make it an initial package

--- data_analysis/data_analysis.py
+++ data_analysis/data_analysis.py
@@ -22,24 +22,24 @@ def _download(location,statistic):
     # Download the content of the URL
     response = requests.get(url)
     # Save it to a file
-    with open("data_{location}-{statistic}.txt", 'w') as open_file:
+    with open(f"data_{location}-{statistic}.txt", 'w') as open_file:
         open_file.write(response.text)
 
 # Load the data using numpy (skip the header records which are marked with a `%`).
 
 def open_data(location,statistic="TAVG"):
-    if not os.path.exists("data_{location}_{statistic}.txt"):
+    if not os.path.exists(f"data_{location}_{statistic}.txt"):
         _download(location,statistic)
     return np.loadtxt("data.txt", comments="%")
 
 def moving_average(data,window):
-    moving_avg = np.full(data, np.nan)
+    moving_avg = np.full(data.size, np.nan)
     for i in range(window, moving_avg.size - window):
-        moving_avg[i] = np.mean(data[i - width:i + width])
+        moving_avg[i] = np.mean(data[i - window:i + window])
     return moving_avg
 
 def testing_moving_average():
-    moving_avg=moving_average(np.ones((1000,1)),4)
-    assert np.all(np.isnan(moving_avg[0:2])),"Head fails"
-    assert np.all(np.isnan(moving_avg[-2:])),"Tail fails"
-    assert np.allclose(moving_avg[2:-2],1),"Body fails"
+    moving_avg=moving_average(np.ones(1000).astype(np.float),4)
+    assert np.all(np.isnan(moving_avg[0:4])),"Head fails"
+    assert np.all(np.isnan(moving_avg[-4:])),"Tail fails"
+    assert np.allclose(moving_avg[4:-4],1.0),"Body fails"
